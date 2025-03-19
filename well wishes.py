e61ac0ee2d8d3d25e34ee637124fb9a4a14eec2c
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-03-19 17:23:24 +0400

    wellwishes

---
+++ well wishes.py
@@ -0,0 +1,4 @@
+def wellwishes():
+    print("Hello, my name is beshoy and i am from egypt but live in dubai and my favorate rap song is sprinter by central cee.")
+    print("hello, it is me again")
+wellwishes()
